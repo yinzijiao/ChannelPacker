00ffc9e0f5973145a29e8c06912e174af57d6ed1
yinzijiao <1527698149@qq.com>
2017-05-19 18:19:44 +0800

    优化UI

--- src/com/test/ui/SettingUI.java
+++ src/com/test/ui/SettingUI.java
@@ -1,7 +1,10 @@
 package com.test.ui;
 
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.options.ConfigurableUi;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.ui.TextComponentAccessor;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.util.text.StringUtil;
 import com.test.Constants;
 import com.test.config.Setting;
@@ -22,63 +25,24 @@ public class SettingUI implements ConfigurableUi<Setting> {
 
     private JComboBox comboBox1;
     private JPanel jPanel;
-    private JTextField textField1;
-    private JButton button1;
     private JLabel hint;
-    private JTextField jKeystore;
     private JTextField jPassword;
     private JLabel password;
     private JLabel keystore;
-    private JTextField jApktools;
     private JLabel apktools;
-    private JButton bKeystore;
-    private JButton bApktools;
     private JTextField jAlias;
     private JLabel alias;
+    private TextFieldWithBrowseButton jFlavor;
+    private TextFieldWithBrowseButton jKeystore;
+    private TextFieldWithBrowseButton jApktools;
 
     public SettingUI() {
         comboBox1.addItem(Constants.CHANNEL_TYPE_META_INF);
         comboBox1.addItem(Constants.CHANNEL_TYPE_MANIFEST);
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                fileChooser.showDialog(new JPanel(), "选择");
-                File selectedFile = fileChooser.getSelectedFile();
-                if (selectedFile != null) {
-                    textField1.setText(selectedFile.getAbsolutePath());
-                    textField1.requestDefaultFocus();
-                }
-            }
-        });
 
-        bKeystore.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                fileChooser.showDialog(new JPanel(), "选择");
-                File selectedFile = fileChooser.getSelectedFile();
-                if (selectedFile != null) {
-                    jKeystore.setText(selectedFile.getAbsolutePath());
-                    jKeystore.requestDefaultFocus();
-                }
-            }
-        });
-        bApktools.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                fileChooser.showDialog(new JPanel(), "选择");
-                File selectedFile = fileChooser.getSelectedFile();
-                if (selectedFile != null) {
-                    jApktools.setText(selectedFile.getAbsolutePath());
-                    jApktools.requestDefaultFocus();
-                }
-            }
-        });
+        jFlavor.addBrowseFolderListener("选择", null, null, new FileChooserDescriptor(true, false, true, false, false, false), TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT, false);
+        jApktools.addBrowseFolderListener("选择", null, null, new FileChooserDescriptor(true, false, true, false, false, false), TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT, false);
+        jKeystore.addBrowseFolderListener("选择", null, null, new FileChooserDescriptor(true, false, true, false, false, false), TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT, false);
 
         hint.setForeground(new Color(255, 107, 104));
 
@@ -99,8 +63,6 @@ public class SettingUI implements ConfigurableUi<Setting> {
             jPassword.setVisible(false);
             apktools.setVisible(false);
             jApktools.setVisible(false);
-            bApktools.setVisible(false);
-            bKeystore.setVisible(false);
             alias.setVisible(false);
             jAlias.setVisible(false);
         } else {
@@ -111,8 +73,6 @@ public class SettingUI implements ConfigurableUi<Setting> {
             jPassword.setVisible(true);
             apktools.setVisible(true);
             jApktools.setVisible(true);
-            bApktools.setVisible(true);
-            bKeystore.setVisible(true);
             alias.setVisible(true);
             jAlias.setVisible(true);
         }
@@ -164,7 +124,7 @@ public class SettingUI implements ConfigurableUi<Setting> {
 
     private void resetChannelFile(@NotNull Setting setting) {
         if (!StringUtil.isEmpty(setting.getState().getChannelFile())) {
-            textField1.setText(setting.getState().getChannelFile());
+            jFlavor.setText(setting.getState().getChannelFile());
         }
     }
 
@@ -195,7 +155,7 @@ public class SettingUI implements ConfigurableUi<Setting> {
     }
 
     private boolean modifiedChannelFile(@NotNull Setting setting) {
-        return !setting.getState().getChannelFile().equals(textField1.getText().toString());
+        return !setting.getState().getChannelFile().equals(jFlavor.getText().toString());
     }
 
     private boolean modifiedKeystore(@NotNull Setting setting) {
@@ -231,7 +191,7 @@ public class SettingUI implements ConfigurableUi<Setting> {
     }
 
     private void applyChannelFile(@NotNull Setting setting) {
-        setting.getState().setChannelFile(textField1.getText().toString());
+        setting.getState().setChannelFile(jFlavor.getText().toString());
     }
 
     private void applyType(@NotNull Setting setting) {
