46ba15b54283f82f50a8ebe20f267aba2933a39b
yinzijiao <1527698149@qq.com>
2017-05-19 17:27:33 +0800

    打包

---
+++ src/com/test/Main.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * Created by yin on 2017/5/19.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+    }
+
+}
