25727212cba35043d6939f9b944e033ac6587d74
yinzijiao <1527698149@qq.com>
2017-05-24 17:05:33 +0800

    支持带下划线和驼峰式命名的翻译

--- src/com/packer/action/YDTranslation.java
+++ src/com/packer/action/YDTranslation.java
@@ -18,54 +18,85 @@ import com.packer.utils.UIHelper;
  */
 public class YDTranslation extends AnAction {
 
+    private Editor editor;
+    private int repeatCount = 0;
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Editor editor = e.getData(LangDataKeys.EDITOR);
+        editor = e.getData(LangDataKeys.EDITOR);
         String text = editor.getSelectionModel().getSelectedText();
         if (!StringUtil.isEmpty(text)) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    String result = null;
-                    try {
-                        result = HttpRequest.sendGet(Constants.YD_BASE_URL, String.format(Constants.YD_URL_PARAM, ResUtil.getUrlEncode(text)));
-                    } catch (Exception e1) {
-                        result = HttpRequest.sendGet(Constants.YD_BASE_URL, String.format(Constants.YD_URL_PARAM, text));
-                    }
-                    Logger.e(result);
-                    YDResultBean resultBean = new Gson().fromJson(result, YDResultBean.class);
-                    if (resultBean != null) {
-                        String showText = "";
-                        switch (resultBean.getErrorCode()) {
-                            case 0:
-                                StringBuilder builder = new StringBuilder();
-                                for (int i = 0; i < resultBean.getTranslation().size(); i++) {
-                                    builder.append(i + 1 + "、" + resultBean.getTranslation().get(i) + "\n");
-                                }
-                                showText = builder.toString();
-                                break;
-                            case 20:
-                                showText = "错误：要翻译的文本过长";
-                                break;
-                            case 30:
-                                showText = "错误：无法进行有效的翻译";
-                                break;
-                            case 40:
-                                showText = "错误：不支持的语言类型";
-                                break;
-                            case 50:
-                                showText = "错误：无效的key";
-                                break;
-                            case 60:
-                                showText = "错误：无词典结果，仅在获取词典结果生效";
-                                break;
-                            default:
-                                break;
-                        }
-                        UIHelper.showPopupBalloon(editor, showText);
-                    }
+                    translation(text);
                 }
             }).start();
         }
+    }
+
+    private void translation(String text) {
+        String result = null;
+        try {
+            result = HttpRequest.sendGet(Constants.YD_BASE_URL, String.format(Constants.YD_URL_PARAM, ResUtil.getUrlEncode(text)));
+        } catch (Exception e1) {
+            result = HttpRequest.sendGet(Constants.YD_BASE_URL, String.format(Constants.YD_URL_PARAM, text));
+        }
+        Logger.e(result);
+        YDResultBean resultBean = new Gson().fromJson(result, YDResultBean.class);
+        if (resultBean != null) {
+            String showText = "";
+            switch (resultBean.getErrorCode()) {
+                case 0:
+                    if (repeatCount < 10 && resultBean.getTranslation() != null && resultBean.getTranslation().size() > 0 && text.equals(resultBean.getTranslation().get(0))) {
+                        translation(createCorrectText(text));
+                        Logger.e(createCorrectText(text) + repeatCount);
+                        repeatCount++;
+                        break;
+                    }
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < resultBean.getTranslation().size(); i++) {
+                        builder.append(i + 1 + "、" + resultBean.getTranslation().get(i) + "\n");
+                    }
+                    showText = builder.toString();
+                    break;
+                case 20:
+                    showText = "错误：要翻译的文本过长";
+                    break;
+                case 30:
+                    showText = "错误：无法进行有效的翻译";
+                    break;
+                case 40:
+                    showText = "错误：不支持的语言类型";
+                    break;
+                case 50:
+                    showText = "错误：无效的key";
+                    break;
+                case 60:
+                    showText = "错误：无词典结果，仅在获取词典结果生效";
+                    break;
+                default:
+                    break;
+            }
+            UIHelper.showPopupBalloon(editor, showText);
+            repeatCount = 0;
+        }
+    }
+
+    private String createCorrectText(String text) {
+        String str = text;
+        if (str.contains("_")) {
+            str = str.replace("_", " ");
+        }
+        if (Math.abs(str.compareTo(str.toLowerCase())) > 0) {
+            int iMax = str.length() + Math.abs(str.compareTo(str.toLowerCase())) / 32;
+            for (int i = 0; i < iMax; i++) {
+                if (str.charAt(i) != str.toLowerCase().charAt(i)) {
+                    str = str.substring(0, i) + " " + str.substring(i);
+                    i++;
+                }
+            }
+        }
+        return str;
     }
 }
